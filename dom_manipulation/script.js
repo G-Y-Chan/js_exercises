cd0c9a43c9e4ec87a869ebbdadd422e345d3904c
Chan Guan Yu <changuanyu2904@gmail.com>
2024-06-21 20:56:53 +0800

    Created description div and appended it to div in html file

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -1,1 +1,7 @@
 const container = document.querySelector("#container");
+
+const description = document.createElement("div");
+description.classList.add("description");
+description.textContent = "Description of webpage here";
+
+container.appendChild(description);
