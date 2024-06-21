6f513cfb374830b9349fc370e913cd7afc940466
Chan Guan Yu <changuanyu2904@gmail.com>
2024-06-21 21:27:39 +0800

    Completed DOM manipulation Exercise_03

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -17,3 +17,20 @@ const blueH3 = document.createElement("h3");
 blueH3.style.color = "blue";
 blueH3.textContent = "I'm a blue h3!";
 container.append(blueH3);
+
+//DOM Manipulation Exercise 03
+const blackPinkDiv = document.createElement("div");
+blackPinkDiv.style.backgroundColor = "pink";
+blackPinkDiv.style.borderColor = "black";
+blackPinkDiv.style.borderWidth = "3";
+blackPinkDiv.style.borderStyle = "solid";
+
+const nestedH1 = document.createElement("h1");
+nestedH1.textContent = "I'm in a div";
+blackPinkDiv.appendChild(nestedH1);
+
+const nestedP = document.createElement("p");
+nestedP.textContent = "ME TOO!";
+blackPinkDiv.appendChild(nestedP);
+
+container.appendChild(blackPinkDiv);
