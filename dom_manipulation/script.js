e0855aaecca1f55b938c9819ae81aacdfe548ae9
Chan Guan Yu <changuanyu2904@gmail.com>
2024-06-21 21:01:57 +0800

    Completed DOM manipulation Exercise_01

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -5,3 +5,9 @@ description.classList.add("description");
 description.textContent = "Description of webpage here";
 
 container.appendChild(description);
+
+//DOM Manipulation Exercise 01
+const redP = document.createElement("p");
+redP.style.color = "red";
+redP.textContent = "Hey I'm red!";
+container.appendChild(redP);
