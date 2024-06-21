f672e3647be563e15dddd3c11af70fef827cdd1a
Chan Guan Yu <changuanyu2904@gmail.com>
2024-06-21 21:05:23 +0800

    Completed DOM maniplation Exercise_02

--- dom_manipulation/script.js
+++ dom_manipulation/script.js
@@ -11,3 +11,9 @@ const redP = document.createElement("p");
 redP.style.color = "red";
 redP.textContent = "Hey I'm red!";
 container.appendChild(redP);
+
+//DOM Manipulation Exercise 02
+const blueH3 = document.createElement("h3");
+blueH3.style.color = "blue";
+blueH3.textContent = "I'm a blue h3!";
+container.append(blueH3);
