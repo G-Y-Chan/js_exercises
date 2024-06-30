da6a36a8eefeed3cb7d8c713becae26ce31b7784
Chan Guan Yu <changuanyu2904@gmail.com>
2024-06-30 09:18:36 +0800

    Created function to add new divs in container div

---
+++ events/script.js
@@ -0,0 +1,11 @@
+function addDiv() {
+    const container = document.querySelector("#container");
+    const newDiv = document.createElement("div");
+    let redValue = Math.floor(Math.random() * 256);
+    let greenValue = Math.floor(Math.random() * 256);
+    let blueValue = Math.floor(Math.random() * 256);
+    newDiv.style.backgroundColor = `rgba(${redValue}, ${greenValue}, ${blueValue}, 1)`;
+    newDiv.style.height = "50px";
+
+    container.appendChild(newDiv);
+}
