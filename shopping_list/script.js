fbad82587e0bb90092801cc0822980145f9050cb
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-01 14:50:27 +0800

    Created function to add items to shopping list and added logic for input retrieval

--- shopping_list/script.js
+++ shopping_list/script.js
@@ -1,3 +1,9 @@
 const itemList = document.querySelector("ul");
 const itemInput = document.querySelector("input");
 const addItemButton = document.querySelector("button");
+
+function addItem() {
+    //Logic to retrieve input value
+    const newItem = itemInput.value;
+    itemInput.value = '';
+}
