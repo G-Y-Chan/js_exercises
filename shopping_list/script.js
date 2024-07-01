7ebc5cca2255154dedb31d0669cfe42e36508b32
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-01 15:09:25 +0800

    Added logic to create and append new item to list

--- shopping_list/script.js
+++ shopping_list/script.js
@@ -6,4 +6,23 @@ function addItem() {
     //Logic to retrieve input value
     const newItem = itemInput.value;
     itemInput.value = '';
+
+    //Check for empty inputs
+    if (newItem === '') return;
+
+    //Create new list item
+    const newListItem = document.createElement("li");
+
+    //Span to contain input value
+    const newSpan = document.createElement("span");
+    newSpan.textContent = newItem;
+
+    //Delete button for list item
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+
+    newListItem.appendChild(newSpan);
+    newListItem.appendChild(deleteButton);
+
+    itemList.appendChild(newListItem);
 }
