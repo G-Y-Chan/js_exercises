fef72f24f90efef630687cec4942aa6117760d1e
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-01 14:27:54 +0800

    Fixed typo to get correct reference to input element

--- shopping_list/script.js
+++ shopping_list/script.js
@@ -1,3 +1,3 @@
 const itemList = document.querySelector("ul");
-const itemInput = document.querySelector("ls");
+const itemInput = document.querySelector("input");
 const addItemButton = document.querySelector("button");
