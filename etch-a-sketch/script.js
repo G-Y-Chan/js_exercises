7982bb7f1e20dab55c2d022f9e6bd57de6d6a167
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-04 11:15:54 +0800

    Created function to create canvas given number of squares per side

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,18 @@
+function create(numSquares) {
+    const container = document.querySelector("#container");
+    for (i = 0; i < numSquares; i++) {
+        const newRow = document.createElement("div");
+        newRow.style.display = "flex";
+        newRow.style.flexDirection = "row";
+        newRow.style.gap = "1px";
+        for (j = 0; j < numSquares; j++) {
+            const newGridSquare = document.createElement("div");
+            newGridSquare.classList.add("gridSquare");
+            newGridSquare.style.backgroundColor = "lightgrey";
+            newGridSquare.style.height = `${90/numSquares}vh`;
+            newGridSquare.style.width = `${90/numSquares}vh`;
+            newRow.appendChild(newGridSquare);
+        }
+        container.appendChild(newRow);
+    }
+}
