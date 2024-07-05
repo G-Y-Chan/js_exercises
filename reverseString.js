47a76d364ee2eba448448df2cb72706a42d928a7
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-05 11:59:28 +0800

    Moved JavaScript exercises into one parent folder

--- reverseString.js
+++
@@ -1,9 +0,0 @@
-const reverseString = function(inputString) {
-    const oldArray = inputString.split("");
-    oldArray.reverse();
-    const newString = oldArray.join("");
-    return newString;
-};
-
-// Do not edit below this line
-module.exports = reverseString;
