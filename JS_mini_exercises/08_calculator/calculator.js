de8087605d4af056059bfef7093e9bb876708ff7
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-08 10:29:17 +0800

    Completed subtract function

--- JS_mini_exercises/08_calculator/calculator.js
+++ JS_mini_exercises/08_calculator/calculator.js
@@ -2,8 +2,8 @@ const add = function(...args) {
 	return args.reduce((a, b) => a+b, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
 const sum = function() {
