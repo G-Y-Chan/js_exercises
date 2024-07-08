587fe0ab7c8ab0e14734f07ba116bc2614ae8377
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-08 10:59:28 +0800

    Completed palindromes exercise

--- JS_mini_exercises/09_palindromes/palindromes.js
+++ JS_mini_exercises/09_palindromes/palindromes.js
@@ -1,3 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let charArray = [...str]
+    .filter(char => /[a-zA-Z0-9]/.test(char))
+    .map(char => char.toLowerCase());
+    
+    for (let i = 0; i < charArray.length; i++) {
+        if (charArray[i] != charArray[charArray.length-1-i]) {
+            return false;
+        }
+    }
+    return true;
 };
