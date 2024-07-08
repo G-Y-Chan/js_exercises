9e017818c27121298279b23d644adc541987f8b3
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-08 16:54:06 +0800

    Created function that calls one of the math functions based on input arguments

--- calculator/script.js
+++ calculator/script.js
@@ -22,3 +22,19 @@ function multiply(a, b) {
 function divide(a, b) {
     return a/b;
 }
+
+//Function that calls one of the above function
+function operate(opr, operand1, operand2) {
+    switch(opr) {
+        case "add":
+            return add(operand1, operand2);
+        case "subtract":
+            return subtract(operand1, operand2);
+        case "multiply":
+            return multiply(operand1, operand2);
+        case "divide":
+            return divide(operand1, operand2);
+        default:
+            return 0;
+    }
+}
