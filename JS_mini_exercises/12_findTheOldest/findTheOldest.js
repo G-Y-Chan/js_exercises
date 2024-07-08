e30839afffae6e9b93a973d01111ef8c6e3f343a
Chan Guan Yu <changuanyu2904@gmail.com>
2024-07-08 11:40:40 +0800

    Completed findTheOldest exercise

--- JS_mini_exercises/12_findTheOldest/findTheOldest.js
+++ JS_mini_exercises/12_findTheOldest/findTheOldest.js
@@ -1,3 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    for (let i = 1; i < people.length; i++) {
+        let currPerson = people[i];
+        if (findAge(currPerson) > findAge(oldest)) {
+            oldest = currPerson;
+        }
+    }
+    return oldest;
 };
+
+function findAge(person) {
+    let currYear = new Date().getFullYear();
+    let finalYear = (person.yearOfDeath === undefined) ? currYear : person.yearOfDeath;
+    return finalYear - person.yearOfBirth;
+}
